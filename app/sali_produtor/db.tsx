421efad954c5c230437f8c8ea1dc88a49a995e0c
Sali Dev <142994380+SaliProdutor@users.noreply.github.com>
2024-04-15 10:11:44 -0300

    Update db.tsx

--- app/sali_produtor/db.tsx
+++ app/sali_produtor/db.tsx
@@ -10,32 +10,58 @@ export const DB = [
                 {
                     id: "02",
                     name: "Design"
+                },
+                {
+                    id: "03",
+                    name: "Dev"
+                },
+                {
+                    id: "04",
+                    name: "Outras Redes"
                 }
             ],
             links:[
                 {
                     id: "01",
+                    categoryId: "04", // corrected property name
+                    name: "LinkedIn",
+                    path: "https://www.linkedin.com/in/saliprodutor/"
+                },
+                {
+                    id: "02",
+                    categoryId: "04", // corrected property name
+                    name: "Site Digital Maker",
+                    path: "https://www.digitalmakerpro.com/"
+                },
+                {
+                    id: "03",
                     categoryId: "01", // corrected property name
                     name: "ULANZI Lapela sem fio J12 para iPhone iPad",
                     path: "https://amzn.to/3JbRSIM"
                 },
                 {
-                    id: "02",
+                    id: "04",
                     categoryId: "01", // corrected property name
                     name: "ULANZI VL49 Pro",
                     path: "https://s.click.aliexpress.com/e/_DEb7w4t"
                 },
                 {
-                    id: "03",
+                    id: "05",
                     categoryId: "01", // corrected property name
                     name: "YONGNUO YN360 III Pro 5500K",
                     path: "https://amzn.to/3xtSHtP"
                 },
                 {
-                    id: "04",
+                    id: "06",
                     categoryId: "02", // corrected property name
                     name: "Portfólio - Behance",
                     path: "https://www.behance.net/sali_produtor"
+                },
+                {
+                    id: "07",
+                    categoryId: "03", // corrected property name
+                    name: "Portfólio - GitHub",
+                    path: "https://github.com/SaliProdutor"
                 },
             ]
         }
