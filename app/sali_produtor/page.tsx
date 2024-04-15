44e2cd872089081db0b4778060dc2ce16fa76911
Sali Dev <142994380+SaliProdutor@users.noreply.github.com>
2024-04-15 09:18:44 -0300

    Update page.tsx

--- app/sali_produtor/page.tsx
+++ app/sali_produtor/page.tsx
@@ -7,7 +7,7 @@ import { motion } from "framer-motion"
 
 export default function Home() {
   return (
-    <main className="flex max-w-screen h-screen z-99 flex-col items-center mt-20">   
+    <main className="flex max-w-screen h-screen z-99 flex-col items-center mt-20 mb-20">   
       <motion.div 
         initial={{ opacity: 0, scale: 0.95}}
         animate={{ opacity: 1, scale: 1 }}
