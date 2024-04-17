9dd5ee19f901aa1bc11ea71c3386c54d12813976
Sali Dev <sali.conceito@gmail.com>
2024-04-17 11:22:54 -0300

    Analytcs and Speed

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Footer } from "./sali_produtor/_components/footer";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +23,8 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body className=" min-h-screen flex flex-1 justify-between flex-col">
+        <Analytics/>
+        <SpeedInsights/>
         {children}
         <Footer/>
       </body> 
