e639ba8fd81a7165b178804a8439f0dc13d4ab07
Sali Dev <sali.conceito@gmail.com>
2024-04-17 17:50:05 -0300

    Update

--- app/sali_produtor/_components/categoria.tsx
+++ app/sali_produtor/_components/categoria.tsx
@@ -16,24 +16,24 @@ export function Categorias({ categoria }: CategoriasProps) {
     return (
         <>
         {category?.name !== "Meus Equipamentos" ?
-            <div>
+            <div className='w-full'>
                 {category && 
                 <h1
                     className="text-gray-100 font-bold mb-4 mt-6 text-center"
                 >{category.name}</h1>}
                 <ul>
                     {filteredLinks.map((link, index) => (
-                            <a href={link.path} className='flex' target="_blank" rel="noopener noreferrer">
-                                <motion.li 
-                                    initial={{ opacity: 0, y: 10 }}
-                                    animate={{ opacity: 1, y: 0 }}
-                                    transition={{ delay: index * .2,duration: .2 }}
-                                    className='flex justify-center items-center mb-4 bg-purple-dark lg:hover:bg-purple duration-1000 lg:hover:shadow-2xl shadow-blue py-4 px-8 rounded-lg' 
-                                    key={index}
-                                >
-                                    <a className=' text-gray-100 text-center'>{link.name}</a>
-                                </motion.li>
-                            </a>
+                        <a href={link.path} className='flex' target="_blank" rel="noopener noreferrer">
+                            <motion.li 
+                                initial={{ opacity: 0, y: 10 }}
+                                animate={{ opacity: 1, y: 0 }}
+                                transition={{ delay: index * .2,duration: .2 }}
+                                className='flex flex-1 justify-center items-center mb-4 bg-purple-dark lg:hover:bg-purple duration-1000 lg:hover:shadow-2xl shadow-blue py-4 px-8 rounded-lg' 
+                                key={index}
+                            >
+                                <a className=' text-gray-100 text-center'>{link.name}</a>
+                            </motion.li>
+                        </a>
                     ))}
                 </ul>
             </div>
