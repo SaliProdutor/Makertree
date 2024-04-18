9303af00ae3f9ada0463bce0ee4401010d19b6b0
Sali Dev <142994380+SaliProdutor@users.noreply.github.com>
2024-04-17 23:46:39 -0300

    Atualizar o footer.tsx

--- app/sali_produtor/_components/footer.tsx
+++ app/sali_produtor/_components/footer.tsx
@@ -6,7 +6,7 @@ export function Footer(){
     const anoAtual = new Date().getFullYear()
     return(
         <footer className="flex relative buttom-0 items-center justify-center bottom-0 h-[2rem] w-full">
-            <span className="text-gray-100 text-sm text-center cursor-default">
+            <span className="text-gray-200 text-sm text-center cursor-default">
                 &copy; {anoAtual === ano ? ano : `${ano} - ${anoAtual}`} <a className=" hover:text-purple duration-1000 cursor-pointer" href="https://digitalmakerpro.com" target="_blank">Digital Maker
                 Pro</a>. Todos os direitos reservados.
             </span>
