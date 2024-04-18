8093f740d522483eae4835629d7e9c842bbc8400
Sali Dev <142994380+SaliProdutor@users.noreply.github.com>
2024-04-18 00:05:18 -0300

    Atualizar o carrossel.tsx

--- app/sali_produtor/_components/carrossel.tsx
+++ app/sali_produtor/_components/carrossel.tsx
@@ -70,7 +70,7 @@ export function Carrossel({ categoria }: CarrosselProps) {
                                 
                                 <Link href={link.path} className='flex-1' target="_blank" rel="noopener noreferrer">
                                     <div className='flex pb-2 flex-col h-full gap-2 items-center justify-start  bg-purple-dark duration-1000 rounded-lg lg:hover:bg-purple lg:hover:shadow-2xl'>
-                                        <div className='max-w-[20rem] max-h-[20rem] '>
+                                        <div className='max-w-[25rem] max-h-[25rem] '>
                                             <Image width={100} height={100} className="w-full aspect-square object-cover bg-cover rounded-lg border-blue border-solid border border-lg p-[.2rem]" src={link.photo} alt="Foto"/>
                                         </div>
                                         <p className=' text-gray-100 text-center p-2'>{link.name}</p>
