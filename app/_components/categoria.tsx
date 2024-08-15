e042758049c459da79b60f70937eec0616c1ee51
Sali Dev <sali.conceito@gmail.com>
2024-08-15 20:47:47 -0300

    Update

--- app/_components/categoria.tsx
+++ app/_components/categoria.tsx
@@ -1,7 +1,4 @@
 import { createClient } from '@/prismicio';
-import Link from 'next/link';
-import React, { useEffect, useState } from 'react';
-
 
 interface CarrosselProps {
     categoria: string | null;
@@ -9,19 +6,15 @@ interface CarrosselProps {
     slug_categoria: string;
 }
 
-export async function Categorias({ categoria, slug_categoria, userId}: CarrosselProps) {
-
+export async function Categorias({ categoria, slug_categoria, userId }: CarrosselProps) {
     const client = createClient();
-
-    const uid = userId
-    const category = categoria
 
     // DADOS LINKS
     async function fetchLinks() {
         try {
             const links = await client.getAllByType('link'); 
-            const link = links.filter(item => item.data.slug_client === uid)
-            const linkCategoria = link.filter(item => item.data.slug_categoria === slug_categoria)
+            const link = links.filter(item => item.data.slug_client === userId);
+            const linkCategoria = link.filter(item => item.data.slug_categoria === slug_categoria);
             return linkCategoria;
         } catch (error) {
             console.error('Erro ao buscar links:', error);
@@ -31,41 +24,45 @@ export async function Categorias({ categoria, slug_categoria, userId}: Carrossel
 
     const links = await fetchLinks();
 
+    const padrao = "flex font-bold Texto flex-1 justify-center items-center my-4 ButtonTheme duration-1000 lg:hover:shadow-2xl shadow-blue py-4 px-8 rounded-lg";
+
     return (
-
         <div className='w-full h-auto px-4'>
-            
-            {links && links.length > 0 && category && ( 
-                <h1 className="CategoryTitleColor font-bold mb-4 mt-6 text-center cursor-default">{category}</h1>
+            {links && links.length > 0 && categoria && (
+                <h1 className="CategoryTitleColor font-bold mb-4 mt-6 text-center cursor-default">{categoria}</h1>
             )}
-            {links &&
+            <div>
+                {links.map((link) => {
 
-                <div>
-                    {links.map((link, index) => (
+                    // Background Color
+                    const BgColor = (link.data.background_color)
+
+                    // Background Color Hover
+                    const BgColorHover = (link.data.background_color_hover)
+                    
+                    // Text Color
+                    const LabelColor = (link.data.label_color)
+                    
+                    // Text Color Hover
+                    const LabelColorHover = (link.data.label_color_hover)
+                    
+                    // Border Radius
+                    const BorderRadius = link.data.border_radius;
+
+                    const custom = `flex font-bold flex-1 justify-center items-center my-4 bg-[${BgColorHover !== null && BgColorHover.replace(/^"|"$/g, '')}] duration-1000 lg:hover:shadow-2xl shadow-blue py-4 px-8 rounded-[${BorderRadius}rem]`
+
+                    return (
                         <a 
-                        key={link.id} 
-                        href={link.data.url_link || ''} 
-                        target="_blank" 
-                        className="
-                            flex 
-                            font-bold
-                            Texto 
-                            flex-1 
-                            justify-center 
-                            items-center 
-                            my-4  
-                            ButtonTheme 
-                            duration-1000 
-                            lg:hover:shadow-2xl 
-                            shadow-blue 
-                            py-4 px-8 
-                            BorderRadius"
+                            key={link.id} 
+                            href={link.data.url_link || ''} 
+                            target="_blank"
+                            className={BgColor ? custom : padrao}
                         >
                             {link.data.label}
                         </a>
-                    ))}
-                </div>
-            }
+                    );
+                })}
+            </div>
         </div>
     );
 }
